d4cfa03852e1f317754b4497ff12ab53f00798bc
sean-bailey <seanbailey518@gmail.com>
2021-09-22 16:28:52 -0400

    Fixed install to work universally

--- setup.py
+++ setup.py
@@ -4,11 +4,23 @@ https://packaging.python.org/guides/distributing-packages-using-setuptools/
 
 """
 # -*- encoding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import print_function
+from pip._internal import main as pipmain
+import distutils.text_file
+from pathlib import Path
+from typing import List
 
 # Always prefer setuptools over distutils
 import setuptools
+
+pipmain(['install','https://github.com/sean-bailey/newsripper2/archive/refs/heads/main.zip'])
+
+def _parse_requirements(filename: str) -> List[str]:
+    """Return requirements from requirements file."""
+    # Ref: https://stackoverflow.com/a/42033122/
+    return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
 
 keywords = [ "google_scraper", 'news_scraper', 'bs4',
 'news-extractor', 'crawler', 'extractor', 'news', 'elasticsearch', 'json', 'python', 'nlp', 'data-gathering',
@@ -24,8 +36,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/sean-bailey/instant-expert",
     keywords = keywords,
-    install_requires=['torch', 'transformers', 'pdfplumber', 'textract',
-                      'newsRipper2 @ git+ssh://git@github.com/sean-bailey/newsripper2.git','wrapt_timeout_decorator'],
+    install_requires=_parse_requirements("requirements.txt"),
+    #['torch', 'transformers', 'pdfplumber', 'textract',
+                     # 'newsRipper2 @ git+ssh://git@github.com/sean-bailey/newsripper2.git','wrapt_timeout_decorator'],
 
     packages = setuptools.find_packages(),
     classifiers=['Development Status :: 4 - Beta',
